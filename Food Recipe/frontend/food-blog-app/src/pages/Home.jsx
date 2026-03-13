8cc9786d3d0ee6b1e224833520cd378e6fd4a7c6
Aayush Basnet <aayush9823@gmail.com>
2026-03-13 14:20:05 +0545

    Food recipe has been added

---
+++ Food Recipe/frontend/food-blog-app/src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import foodRecipe from '../assets/foodRecipe.png'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import RecipeItems from '../components/RecipeItems'
+import { useNavigate } from 'react-router-dom'
+import Modal from '../components/Modal'
+import InputForm from '../components/InputForm'
+
+export default function Home() {
+    const navigate = useNavigate()
+    const [isOpen, setIsOpen] = useState(false)
+
+    const addRecipe = () => {
+        let token = localStorage.getItem("token")
+        if (token)
+            navigate("/addRecipe")
+        else {
+            setIsOpen(true)
+        }
+    }
+
+    return (
+        <>
+            <section className='home'>
+                <div className='left'>
+                    <h1>Food Recipe</h1>
+                    <h5>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</h5>
+                    <button onClick={addRecipe}>Share your recipe</button>
+                </div>
+                <div className='right'>
+                    <img src={foodRecipe} width="320px" height="300px"></img>
+                </div>
+            </section>
+            
+            {(isOpen) && <Modal onClose={() => setIsOpen(false)}><InputForm setIsOpen={() => setIsOpen(false)} /></Modal>}
+            <div className='recipe'>
+                <RecipeItems />
+            </div>
+        </>
+    )
+}
